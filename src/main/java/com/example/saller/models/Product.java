f2a0e8af52c87c8dae8f01317f1b2a99d747bdf2
aleksej <dr1ma@icloud.com>
2023-06-20 02:48:23 +0600

    add image

--- src/main/java/com/example/saller/models/Product.java
+++ src/main/java/com/example/saller/models/Product.java
@@ -1,16 +1,21 @@
 package com.example.saller.models;
 
+
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "products")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,10 +25,26 @@ public class Product {
     private String title;
     @Column(name = "description", columnDefinition = "text")
     private String description;
+    @Column(name = "price")
+    private int price;
     @Column(name = "city")
     private String city;
     @Column(name = "author")
     private String author;
-    @Column(name = "price")
-    private int price;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
+            mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
+
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
 }
